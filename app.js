ae77368a75f9ea6c13335c28176b0b355ec578a7
lmbula <30441108+lmbula@users.noreply.github.com>
2023-05-04 14:33:51 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+//Math
+//in built objects always available
+
+//Math.floor() rounds down a number
+
+// const number = 3.6789;
+// const result = Math.floor(number);
+
+//Math.ceil rounds up a number
+
+// const number = 3.6789;
+// const result = Math.ceil(number);
+
+// const number = 100;
+// const result = Math.sqrt(number);
+
+// const result = Math.PI;
+// const result = Math.min(2,3,4,5,6,7,8);
+// const result = Math.max(1000,200,400,9000);
+
+//const result = Math.random(); //result to 0.000 to 0.9999
+//const result = Math.random()*10; // returns a whole number with decimal
+//const result = Math.floor(Math.random()*10); // returns a rounded down number 0- 9
+//const result = Math.ceil(Math.random()*10); // returns a rounded up number from 1-10
+
+const result = Math.floor(Math.random() * 10 + 1); //returns numbers between 1-10
+
+console.log(result);
